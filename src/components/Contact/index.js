30e7e16e20893b2977707978b35973d8a9ae0fb9
dedrie p <dedrie92@hotmail.com>
2022-06-17 19:11:29 -0400

    addded target blank and clickable links on contact

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,15 +1,24 @@
 import React from "react";
 
 
-function Contact (){
+function Contact() {
 
     return (
         <div id="contact" className='mainContent'>
             <h2>
                 How to Reach Me
             </h2>
-            <p>Email: Dedrie.Peart@gmail.com</p>
-            <p>github: github.com/dedriep</p>
+            <p> Email:
+                <a href="mailto: dedrie.peart@gmail.com"> dedrie.peart@gmail.com</a>
+
+            </p>
+
+            <p>Github: 
+            <a href = "github.com/dedriep">Github</a>
+
+                
+                
+                </p>
 
         </div>
     )
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -8,26 +8,26 @@ function Projects(){
             <h2>Things that I have worked on</h2>
             <ul className='projectList'>
                 <li>
-                    <a href= 'https://github.com/Dedriep/read-me-project'> Read-Me Generator
+                    <a href= 'https://github.com/Dedriep/read-me-project' target='_blank' rel="noopener noreferrer"> Read-Me Generator
 </a> 
                 </li>
                 <li>
-                    <a href= 'https://github.com/Dedriep/Budget--Tracker'> Budget Tracker
-</a> 
-                </li>
-
-                <li>
-                    <a href= 'https://github.com/Dedriep/employee-tracker'> Employee Tracker
+                    <a href= 'https://github.com/Dedriep/Budget--Tracker' target='_blank'rel="noopener noreferrer"> Budget Tracker
 </a> 
                 </li>
 
                 <li>
-                    <a href= 'https://github.com/Dedriep/note-taker'> Note Taker
+                    <a href= 'https://github.com/Dedriep/employee-tracker' target='_blank' rel="noopener noreferrer"> Employee Tracker
 </a> 
                 </li>
 
                 <li>
-                    <a href= 'https://github.com/Dedriep/Weather-Finder'> Weather App
+                    <a href= 'https://github.com/Dedriep/note-taker' target='_blank'rel="noopener noreferrer"> Note Taker
+</a> 
+                </li>
+
+                <li>
+                    <a href= 'https://github.com/Dedriep/Weather-Finder' target='_blank'rel="noopener noreferrer"> Weather App
 </a> 
                 </li>
 
