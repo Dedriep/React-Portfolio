4dcc2736f6b8ca827ecec00e918ced4a25df4df5
dedrie p <dedrie92@hotmail.com>
2022-06-17 20:31:20 -0400

    removed attempted fix

--- src/index.js
+++ src/index.js
@@ -7,10 +7,8 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-        <HashRouter>
-        <App />
-
-          </HashRouter>
+    c
+    <App />
   </React.StrictMode>
 );
 
